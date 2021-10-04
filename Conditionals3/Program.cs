33b3da0c585350c4a29d6af59b04f53aad2e780a
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-04 12:50:23 -0300

    added isAlive

--- Conditionals3/Program.cs
+++ Conditionals3/Program.cs
@@ -8,8 +8,26 @@ namespace Conditionals3
 {
 	class Program
 	{
+		static bool isAlive = true;
+
+
+
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Conditionals3");
+
+			isAlive = false;
+			//if (Alive == true)
+			if (isAlive)
+			{
+				Console.WriteLine("Your are alive");
+			}
+			else
+			{
+				Console.WriteLine("You are dead");
+			}
+
+			Console.ReadKey(true);
 		}
 	}
 }
